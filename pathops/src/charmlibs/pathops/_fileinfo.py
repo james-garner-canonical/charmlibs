a8f7d9d6b03e794d0880e480a7a91b9450360955
James Garner <james.garner@canonical.com>
2025-04-15 12:01:54 +1200

    fix: handle permission error in fileinfo from container path

--- pathops/src/charmlibs/pathops/_fileinfo.py
+++ pathops/src/charmlibs/pathops/_fileinfo.py
@@ -46,10 +46,11 @@ _FT_MAP: dict[int, pebble.FileType] = {
 def from_container_path(path: ContainerPath) -> pebble.FileInfo:
     try:
         (info,) = path._container.list_files(path._path, itself=True)
-    except pebble.APIError as e:
+    except (pebble.APIError, pebble.PathError) as e:
         msg = repr(path)
         _errors.raise_if_matches_file_not_found(e, msg=msg)
         _errors.raise_if_matches_not_a_directory(e, msg=msg)
+        _errors.raise_if_matches_permission(e, msg=msg)
         _errors.raise_if_matches_too_many_levels_of_symlinks(e, msg=msg)
         raise
     return info
