696b3ce5714c0f512a20287f63b8a5caa1c16c23
James Garner <james.garner@canonical.com>
2025-05-28 16:25:09 +1200

    ci: simplify python script

--- .github/get-packages.py
+++ .github/get-packages.py
@@ -28,52 +28,41 @@ import string
 import subprocess
 
 _ALPHABET = tuple(string.ascii_lowercase)
-_GLOBAL_FILES = ('pyproject.toml', 'justfile', '.github')
+_GLOBAL_FILES = ('.github', 'justfile', 'pyproject.toml')
 
 
-def _parse_args() -> str | None:
+def _main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('git_base_ref', default=None)
     args = parser.parse_args()
-    return args.git_base_ref
-
-
-def _main(project_root: pathlib.Path, git_base_ref: str | None) -> None:
-    packages = _get_changed_packages(project_root=project_root, git_base_ref=git_base_ref)
-    _set_output(packages)
-
-
-def _get_changed_packages(project_root: pathlib.Path, git_base_ref: str | None) -> list[str]:
-    all_packages = sorted([
-        '_charmlibs',
-        *(
-            path.name
-            for path in project_root.iterdir()
-            if path.is_dir() and path.name.startswith(_ALPHABET)
-        ),
-    ])
-    if not git_base_ref:
-        print('Using all packages because no git base ref was provided.')
-        return all_packages
-    git_diff_cmd = ['git', 'diff', '--name-only', f'origin/{git_base_ref}']
-    git_diff = subprocess.run(git_diff_cmd, capture_output=True, text=True, cwd=project_root)
-    changes = git_diff.stdout.split('\n')
-    names = {change.split('/')[0] for change in changes}
-    # record which packages have changed, or all if global config files have changed
-    global_changes = sorted(name for name in names if name in _GLOBAL_FILES)
-    if global_changes:
-        print(f'Using all packages because global files were changed: {global_changes}')
-        return all_packages
-    print(f'Using packages that are changed compared to {git_base_ref}.')
-    return [p for p in all_packages if p in names]
-
-
-def _set_output(packages: list[str]) -> None:
+    packages = _get_changed_packages(project_root=pathlib.Path(), git_base_ref=args.git_base_ref)
+    line = f'packages={json.dumps(packages)}'
+    print(line)
     with pathlib.Path(os.environ['GITHUB_OUTPUT']).open('a') as f:
-        line = f'packages={json.dumps(packages)}'
-        print(line)
         print(line, file=f)
 
 
+def _get_changed_packages(project_root: pathlib.Path, git_base_ref: str | None) -> list[str]:
+    all_packages = sorted(
+        path.name
+        for path in project_root.iterdir()
+        if path.is_dir() and (path.name.startswith(_ALPHABET) or path.name == '_charmlibs')
+    )
+    if not git_base_ref:
+        print('Using all packages because no git base ref was provided.')
+        return all_packages
+    cmd = ['git', 'diff', '--name-only', f'origin/{git_base_ref}']
+    diff = subprocess.check_output(cmd, text=True, cwd=project_root)
+    changes = {path.split('/')[0] for path in diff.split('\n')}
+    # record which packages have changed, or all if global config files have changed
+    global_changes = [f for f in _GLOBAL_FILES if f in changes]
+    if global_changes:
+        print(f'Using all packages because global files were changed: {global_changes}')
+        return all_packages
+    changed_packages = [p for p in all_packages if p in changes]
+    print(f'Using packages that are changed compared to {git_base_ref}: {changed_packages}')
+    return changed_packages
+
+
 if __name__ == '__main__':
-    _main(project_root=pathlib.Path(), git_base_ref=_parse_args())
+    _main()
