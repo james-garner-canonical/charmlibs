6c3fb6f2f42f19baa8451cbe5475fc58d42fa0ec
James Garner <james.garner@canonical.com>
2025-06-13 16:25:19 +1200

    docs: clean up generate.py

--- _docs/generate.py
+++ _docs/generate.py
@@ -37,15 +37,30 @@ _EMOJIS = {
     # 'docs': '📚',
     # 'src': '⌨️',
 }
-_STATUS_DESCRIPTIONS = {
+_STATUS_TOOLTIPS = {
     'recommended': 'Recommended for use in new charms today!',
     'dep': 'Dependency of other libs, unlikely to be needed directly.',
     'legacy': 'There are better alternatives available.',
     'team': 'Team internal lib, may not be stable for external use.',
 }
-_KIND_PRIORITIES = {'PyPI': 0, 'git': 1, 'Charmhub': 2}
-_STATUS_PRIORITIES = {s: i for i, s in enumerate(('recommended', 'dep', '', 'legacy', 'team'))}
-_SUBSTRATE_PRIORITIES = {'K8s': 0, 'machine': 1, '': 2}
+_KIND_SORTKEYS = {'PyPI': 0, 'git': 1, 'Charmhub': 2, '': 3}
+_STATUS_SORTKEYS = {s: i for i, s in enumerate(('recommended', 'dep', '', 'legacy', 'team'))}
+_SUBSTRATE_SORTKEYS = {'K8s': 0, 'machine': 1, '': 2}
+_TABLE_HEADER_TEMPLATE = """..
+    This file was automatically generated.
+    It should not be manually edited!
+    Instead, edit {csv_file} and then run {script_file}
+
+.. list-table::
+   :class: sphinx-datatable
+   :widths: 1, 40, 1, 60
+   :header-rows: 1
+
+   * -
+     - name
+     - kind
+     - description
+"""
 
 
 class _CSVRow(typing.TypedDict, total=True):
@@ -64,23 +79,7 @@ def _generate_non_relation_libs_table():
     raw = pathlib.Path('reference/non-relation-libs-raw.csv')
     with raw.open() as f:
         entries: list[_CSVRow] = list(csv.DictReader(f))  # type: ignore
-    chunks = [
-        f"""..
-    This file was automatically generated.
-    It should not be manually edited!
-    Instead, edit {raw} and then run {pathlib.Path(__file__).name}
-
-.. list-table::
-   :class: sphinx-datatable
-   :widths: 1, 40, 1, 60
-   :header-rows: 1
-
-   * -
-     - name
-     - kind
-     - description
-""",
-    ]
+    chunks = [_TABLE_HEADER_TEMPLATE.format(csv_file=raw, script_file=pathlib.Path(__file__).name)]
     rows = [(_status(entry), _name(entry), _kind(entry), _description(entry)) for entry in entries]
     for row in sorted(rows, key=lambda r: (r[0], r[2], r[3], r[1])):  # sort: status, kind, desc
         first, *rest = (f' {cell}' if cell and not cell.startswith('\n') else cell for cell in row)
@@ -91,37 +90,36 @@ def _generate_non_relation_libs_table():
 
 def _status(entry: _CSVRow) -> str:
     status = entry['status']
-    prefix = _hidden_text(_STATUS_PRIORITIES[status])
+    prefix = _hidden_text(_STATUS_SORTKEYS[status])
     if status not in _EMOJIS:
         return prefix
-    if status not in _STATUS_DESCRIPTIONS:
+    if status not in _STATUS_TOOLTIPS:
         return f'{prefix}       | {_EMOJIS[status]}'
     return (
         prefix.rstrip()
         + '\n          '
         + f'<div class="emoji-div">{_EMOJIS[status]}'
-        + f'<div class="emoji-tooltip">{_STATUS_DESCRIPTIONS[status]}</div>'
+        + f'<div class="emoji-tooltip">{_STATUS_TOOLTIPS[status]}</div>'
         + '</div>'
         + '\n\n'
     )
 
 
-def _name(entry: _CSVRow, one_line: bool = True) -> str:
-    name = _rst_link(entry['name'], entry['url'])
-    assert name
-    urls = {f'{_EMOJIS.get(k, "")}{k}': entry[k] for k in ('docs', 'src')}
-    links = [_rst_link(k, v) for k, v in urls.items() if v]
-    if not links:
-        return name
-    links_str = ', '.join(links)
-    if one_line:
-        return f'{name} ({links_str})'
-    return f'| {name}\n       | ({links_str})'
+def _name(entry: _CSVRow) -> str:
+    main_link = _rst_link(entry['name'], entry['url'])
+    extra_links = ', '.join([
+        _rst_link(_EMOJIS.get(text, '') + text, url)
+        for text in ('docs', 'src')
+        if (url := entry[text])
+    ])
+    if not extra_links:
+        return main_link
+    return f'{main_link} ({extra_links})'
 
 
 def _kind(entry: _CSVRow) -> str:
     kind = entry['kind']
-    prefix = _hidden_text(_KIND_PRIORITIES[kind])
+    prefix = _hidden_text(_KIND_SORTKEYS[kind])
     kind_str = _EMOJIS.get(kind, '') + kind
     if not kind_str:
         return prefix
@@ -130,10 +128,10 @@ def _kind(entry: _CSVRow) -> str:
 
 def _description(entry: _CSVRow) -> str:
     substrates = ('machine', 'K8s')
-    priorities = ''.join(str(_SUBSTRATE_PRIORITIES[s if entry[s] else '']) for s in substrates)
+    priorities = ''.join(str(_SUBSTRATE_SORTKEYS[s if entry[s] else '']) for s in substrates)
     prefix = _hidden_text(priorities)
     substrate_line = ' '.join(_EMOJIS.get(s, '') + s for s in substrates if entry[s])
-    description = '\n'.join(x for x in (substrate_line, entry['description']) if x)
+    description = '\n'.join(s for s in (substrate_line, entry['description']) if s)
     if not description:
         return prefix
     description_str = description.replace('\n', '\n       | ')
