267cf0e50215f59b49ee296b572f80526f2fd7ba
James Garner <james.garner@canonical.com>
2025-08-07 17:14:34 +1200

    docs: correct extension and update commited files and ignores

--- .docs/extensions/package_docs.py
+++ .docs/extensions/package_docs.py
@@ -70,9 +70,9 @@ def _generate_files(docs_dir: str | pathlib.Path) -> None:
         file_contents = AUTOMODULE_TEMPLATE.format(
             package=package.name, underline='=' * len(package.name)
         )
-        _write_if_needed(path=(reference_dir / f'{package}.rst'), contents=file_contents)
+        _write_if_needed(path=(reference_dir / f'{package}.rst'), content=file_contents)
     index_contents = INDEX_TEMPLATE.format(packages='\n'.join(packages))
-    _write_if_needed(path=(reference_dir / 'index.md'), contents=index_contents)
+    _write_if_needed(path=(reference_dir / 'index.md'), content=index_contents)
 
 
 def _write_if_needed(path: pathlib.Path, content: str) -> None:
