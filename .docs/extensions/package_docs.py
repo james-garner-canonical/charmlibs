6327719339f5c74c73012150e9889f0af1b5f1dd
James Garner <james.garner@canonical.com>
2025-08-14 13:42:15 +1200

    docs: use the full import package name as the page title
    We use the base import package name for the TOC, this may change if we
    settle on a convention for the distribution package name.

--- .docs/extensions/package_docs.py
+++ .docs/extensions/package_docs.py
@@ -42,6 +42,14 @@ def _package_docs(app: sphinx.application.Sphinx) -> None:
 ####################
 
 AUTOMODULE_TEMPLATE = """
+.. raw:: html
+
+   <style>
+      h1:before {{
+         content: "{prefix}";
+      }}
+   </style>
+
 {package}
 {underline}
 
@@ -58,13 +66,17 @@ def _generate_files(docs_dir: pathlib.Path, subdir: str = '.', exclude: str | No
     generated_dir = docs_dir / 'reference' / 'charmlibs' / subdir
     generated_dir.mkdir(exist_ok=True)
     # Any directory starting with a-z is assumed to be a package (except the interfaces directory)
-    packages = sorted(
+    package_dir_names = sorted(
         path.name
         for path in (docs_dir.parent / subdir).glob(r'[a-z]*')
         if path.is_dir() and path.name != exclude
     )
-    for package in packages:
-        automodule = AUTOMODULE_TEMPLATE.format(package=package, underline='=' * len(package))
+    for package_dir_name in package_dir_names:
+        prefix = 'charmlibs.' if subdir == '.' else f'charmlibs.{subdir}.'
+        package = package_dir_name.replace('-', '_')
+        automodule = AUTOMODULE_TEMPLATE.format(
+            prefix=prefix, package=package, underline='=' * len(package)
+        )
         _write_if_needed(path=generated_dir / f'{package}.rst', content=automodule)
 
 
