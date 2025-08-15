98f7ac97215d27cef8c585464a796cd9ef8a28c2
James Garner <james.garner@canonical.com>
2025-08-15 22:41:46 +1200

    docs: update conf.py, add sitemap and last modified (#114)
    This PR updates .docs/conf.py to clean things up and add the sitemap and last modified features from the latest docs starter pack.

--- .docs/conf.py
+++ .docs/conf.py
@@ -1,5 +1,4 @@
 import datetime
-import ast
 import pathlib
 import sys
 
@@ -23,22 +22,6 @@ sys.path[0:0] = [
     ),
 ]
 
-def setup(app):
-    app.add_css_file('project_specific.css')
-
-# don't automatically add parentheses after function and method references
-add_function_parentheses = False
-
- # not worthwhile because it doesn't replace references in function signatures
- # open issue: https://github.com/sphinx-doc/sphinx/issues/10785
- # autodoc_type_aliases = {'StrPathLike': 'str | os.PathLike[str]'}
-
-# Configuration for the Sphinx documentation builder.
-# All configuration specific to your project should be done in this file.
-#
-# If you're new to Sphinx and don't want any advanced or custom features,
-# just go through the items marked 'TODO'.
-#
 # A complete list of built-in Sphinx configuration values:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 #
@@ -46,163 +29,53 @@ add_function_parentheses = False
 # to keep all documentation based on it consistent and on brand:
 # https://github.com/canonical/canonical-sphinx
 
-
 #######################
 # Project information #
 #######################
 
 # Project name
-#
-# TODO: [@dwilding DONE] Update with the official name of your project or product
-
 project = "Charmlibs"
 author = "Canonical Ltd."
-
-
-# Sidebar documentation title; best kept reasonably short
-#
-# TODO: To include a version number, add it here (hardcoded or automated).
-#
-# TODO: To disable the title, set to an empty string.
-
-html_title = project + " documentation"
-
-
-# Copyright string; shown at the bottom of the page
-#
-# Now, the starter pack uses CC-BY-SA as the license
-# and the current year as the copyright year.
-#
-# TODO: If your docs need another license, specify it instead of 'CC-BY-SA'.
-#
-# TODO: [@dwilding DONE] If your documentation is a part of the code repository of your project,
-#       it inherits the code license instead; specify it instead of 'CC-BY-SA'.
-#
-# NOTE: For static works, it is common to provide the first publication year.
-#       Another option is to provide both the first year of publication
-#       and the current year, especially for docs that frequently change,
-#       e.g. 2022–2023 (note the en-dash).
-#
-#       A way to check a repo's creation date is to get a classic GitHub token
-#       with 'repo' permissions; see https://github.com/settings/tokens
-#       Next, use 'curl' and 'jq' to extract the date from the API's output:
-#
-#       curl -H 'Authorization: token <TOKEN>' \
-#         -H 'Accept: application/vnd.github.v3.raw' \
-#         https://api.github.com/repos/canonical/<REPO> | jq '.created_at'
-
-copyright = "%s, %s" % (datetime.date.today().year, author)
-
+slug = 'charmlibs'  # https://meta.discourse.org/t/what-is-category-slug/87897
+html_title = f"{project} documentation"  # sidebar documentation title
+copyright = f"{datetime.date.today().year} CC-BY-SA, {author}"  # shown at the bottom of the page
 
 # Documentation website URL
-#
-# TODO: [@dwilding DONE] Update with the official URL of your docs or leave empty if unsure.
-#
-# NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
-#       and is used by social media platforms; see https://ogp.me/
-
 ogp_site_url = "https://documentation.ubuntu.com/charmlibs/"
-
-
 # Preview name of the documentation website
-#
-# TODO: To use a different name for the project in previews, update as needed.
-
 ogp_site_name = project
-
-
 # Preview image URL
-#
-# TODO: To customise the preview image, update as needed.
-
 ogp_image = "https://assets.ubuntu.com/v1/253da317-image-document-ubuntudocs.svg"
 
-
-# Product favicon; shown in bookmarks, browser tabs, etc.
-
-# TODO: To customise the favicon, uncomment and update as needed.
-
-# html_favicon = '.sphinx/_static/favicon.png'
-
-
-# Dictionary of values to pass into the Sphinx context for all pages:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_context
-
 html_context = {
-    # Product page URL; can be different from product docs URL
-    #
-    # TODO: [@dwilding DONE] Change to your product website URL,
-    #       dropping the 'https://' prefix, e.g. 'ubuntu.com/lxd'.
-    #
-    # TODO: If there's no such website,
-    #       remove the {{ product_page }} link from the page header template
-    #       (usually .sphinx/_templates/header.html; also, see README.rst).
     "product_page": "github.com/canonical/charmlibs",
-    # Product tag image; the orange part of your logo, shown in the page header
-    #
-    # TODO: [@dwilding DONE] To add a tag image, uncomment and update as needed.
     'product_tag': '_static/logos/juju-logo-no-text.png',
-    # Your Discourse instance URL
-    #
-    # TODO: [@dwilding DONE] Change to your Discourse instance URL or leave empty.
-    #
-    # NOTE: If set, adding ':discourse: 123' to an .rst file
-    #       will add a link to Discourse topic 123 at the bottom of the page.
     "discourse": "https://discourse.charmhub.io/",
-    # Your Mattermost channel URL
-    #
-    # TODO: [@dwilding DONE] Change to your Mattermost channel URL or leave empty.
+    "discourse_prefix": "https://discourse.charmhub.io/t/",
     "mattermost": "",
-    # Your Matrix channel URL
-    #
-    # TODO: [@dwilding DONE] Change to your Matrix channel URL or leave empty.
     "matrix": "https://matrix.to/#/#charmhub-charmdev:ubuntu.com",
-    # Your documentation GitHub repository URL
-    #
-    # TODO: [@dwilding DONE] Change to your documentation GitHub repository URL or leave empty.
-    #
-    # NOTE: If set, links for viewing the documentation source files
-    #       and creating GitHub issues are added at the bottom of each page.
     "github_url": "https://github.com/canonical/charmlibs",
-    # Docs branch in the repo; used in links for viewing the source files
-    #
-    # TODO: To customise the branch, uncomment and update as needed.
     'repo_default_branch': 'main',
-    # Docs location in the repo; used in links for viewing the source files
-    #
-
-
-    # TODO: To customise the directory, uncomment and update as needed.
     "repo_folder": "/.docs/",
-    # TODO: To enable or disable the Previous / Next buttons at the bottom of pages
-    # Valid options: none, prev, next, both
-    # "sequential_nav": "both",
-    # TODO: To enable listing contributors on individual pages, set to True
     "display_contributors": False,
-
-    # Required for feedback button
-    'github_issues': 'enabled',
+    'github_issues': 'enabled',  # Required for feedback button
 }
-
-# To enable the edit button (pencil icon next to the feedback button)
-#
-# html_theme_options = {
-# 'source_edit_link': 'https://github.com/canonical/charmtech-charmlibs',
-# }
-
-# Project slug; see https://meta.discourse.org/t/what-is-category-slug/87897
-#
-# TODO: If your documentation is hosted on https://docs.ubuntu.com/,
-#       uncomment and update as needed.
-
-slug = 'charmlibs'
-
-
 # Template and asset locations
-
 html_static_path = [".sphinx/_static"]
 templates_path = [".sphinx/_templates"]
 
+# Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
+html_baseurl = 'https://documentation.ubuntu.com/charmlibs/'
+sitemap_url_scheme = '{link}'  # URL scheme. Add language and version scheme elements.
+sitemap_show_lastmod = True  # Include `lastmod` dates in the sitemap:
+sitemap_excludes = [  # Exclude generated pages from the sitemap:
+    '404/',
+    'genindex/',
+    'py-modindex/',
+    'search/',
+   'reference/generated/*',
+]
 
 #############
 # Redirects #
@@ -219,22 +92,16 @@ templates_path = [".sphinx/_templates"]
 
 redirects = {}
 
-
 ###########################
 # Link checker exceptions #
 ###########################
 
 # A regex list of URLs that are ignored by 'make linkcheck'
-#
-# TODO: [@dwilding DONE] Remove or adjust the ACME entry after you update the contributing guide
-
 linkcheck_ignore = [
     "http://127.0.0.1:8000"
 ]
 
-
 # A regex list of URLs where anchors are ignored by 'make linkcheck'
-
 linkcheck_anchors_ignore_for_url = [r"https://github\.com/.*"]
 
 # give linkcheck multiple tries on failure
@@ -244,18 +111,6 @@ linkcheck_retries = 3
 ########################
 # Configuration extras #
 ########################
-
-# Custom MyST syntax extensions; see
-# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
-#
-# NOTE: By default, the following MyST extensions are enabled:
-#       substitution, deflist, linkify
-
-# myst_enable_extensions = set()
-
-
-# Custom Sphinx extensions; see
-# https://www.sphinx-doc.org/en/master/usage/extensions/index.html
 
 # NOTE: The canonical_sphinx extension is required for the starter pack.
 #       It automatically enables the following extensions:
@@ -271,7 +126,6 @@ linkcheck_retries = 3
 #       - sphinxext.opengraph
 #       - terminal-output
 #       - youtube-links
-
 extensions = [
     "canonical_sphinx",
     "sphinxcontrib.cairosvgconverter",
@@ -280,8 +134,15 @@ extensions = [
     'sphinx.ext.intersphinx',
     "sphinx.ext.napoleon",
     "sphinx_datatables",
+    "sphinx_sitemap",
     *local_extensions,
 ]
+
+# Custom MyST syntax extensions; see
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+# NOTE: By default, the following MyST extensions are enabled:
+#       substitution, deflist, linkify
+# myst_enable_extensions = set()
 
 intersphinx_mapping = {
     'ops': ('https://documentation.ubuntu.com/ops/latest', None),
@@ -291,55 +152,25 @@ intersphinx_mapping = {
 }
 
 maximum_signature_line_length = 80
+add_function_parentheses = False  # don't automatically add parentheses after func and method refs
+# disable_feedback_button = True  # Feedback button at the top; enabled by default
 
 # Excludes files or directories from processing
-
 exclude_patterns = [
     "doc-cheat-sheet*",
 ]
 
 # Adds custom CSS files, located under 'html_static_path'
-
 html_css_files = [
-    "css/pdf.css",
+    "project_specific.css",
 ]
 
-
 # Adds custom JavaScript files, located under 'html_static_path'
-
 # html_js_files = []
-
-
-# Specifies a reST snippet to be appended to each .rst file
-#
-# [@dwilding] Commented this out.
-
-# rst_epilog = """
-# .. include:: /reuse/links.txt
-# """
-
-# Feedback button at the top; enabled by default
-#
-# TODO: To disable the button, uncomment this.
-
-# disable_feedback_button = True
-
-
-# Your manpage URL
-#
-# TODO: To enable manpage links, uncomment and update as needed.
-#
-# NOTE: If set, adding ':manpage:' to an .rst file
-#       adds a link to the corresponding man section at the bottom of the page.
-
-# manpages_url = f'https://manpages.ubuntu.com/manpages/{codename}/en/' + \
-#     f'man{section}/{page}.{section}.html'
-
 
 # Specifies a reST snippet to be prepended to each .rst file
 # This defines a :center: role that centers table cell content.
 # This defines a :h2: role that styles content for use with PDF generation.
-
 rst_prolog = """
 .. role:: center
    :class: align-center
@@ -351,44 +182,16 @@ rst_prolog = """
     :class: vale-ignore
 """
 
-# Workaround for https://github.com/canonical/canonical-sphinx/issues/34
-
-if "discourse_prefix" not in html_context and "discourse" in html_context:
-    html_context["discourse_prefix"] = html_context["discourse"] + "/t/"
-
+# Specifies a reST snippet to be appended to each .rst file
+# rst_epilog = """
+# .. include:: /reuse/links.txt
+# """
 
 # Options for sphinx.ext.autodoc
-
-# This value controls how to represents typehints. The setting takes the
-# following values:
-#     'signature' – Show typehints as its signature (default)
-#     'description' – Show typehints as content of function or method
-#     'none' – Do not show typehints
 autodoc_typehints = 'signature'
-
-# This value selects what content will be inserted into the main body of an
-# autoclass directive. The possible values are:
-#     'class' - Only the class’ docstring is inserted. This is the
-#               default. You can still document __init__ as a separate method
-#               using automethod or the members option to autoclass.
-#     'both' - Both the class’ and the __init__ method’s docstring are
-#              concatenated and inserted.
-#     'init' - Only the __init__ method’s docstring is inserted.
 autoclass_content = 'class'
-
-# This value selects if automatically documented members are sorted
-# alphabetical (value 'alphabetical'), by member type (value
-# 'groupwise') or by source order (value 'bysource'). The default is
-# alphabetical.
-#autodoc_member_order = 'alphabetical'
-#autodoc_member_order = 'groupwise'
 autodoc_member_order = 'bysource'
-
-# don't evaluate default values
-# autodoc_preserve_defaults = True
-
 add_module_names = False
-
 autodoc_default_options = {
     'members': None,  # None here means "yes"
     'special-members': None,  # meaning all
@@ -420,10 +223,9 @@ autodoc_default_options = {
     'show-inheritance': None,
 }
 
-# set the version to use for DataTables plugin
-datatables_version = "1.13.4"
-# name of the class to use for tables to enable DataTables
-datatables_class = "sphinx-datatable"
+# Options for sphinx-datatables
+datatables_version = "1.13.4"  # set the version to use for DataTables plugin
+datatables_class = "sphinx-datatable"  # name of the class to use for tables to enable DataTables
 # any custom options to pass to the DataTables constructor
 # any options set are used for all DataTables
 datatables_options = {
