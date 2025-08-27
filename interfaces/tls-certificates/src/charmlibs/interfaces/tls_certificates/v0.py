ee6ae8e2d04007c2ccb0ee3ef201d93f375c0087
James Garner <james.garner@canonical.com>
2025-08-27 14:16:48 +1200

    docs: update the v0 module docstring

--- interfaces/tls-certificates/src/charmlibs/interfaces/tls_certificates/v0.py
+++ interfaces/tls-certificates/src/charmlibs/interfaces/tls_certificates/v0.py
@@ -1,85 +1,65 @@
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-"""Library for the tls-certificates relation.
+"""Module for the ``tls_certificates`` interface v0.
 
-This library contains the Requires and Provides classes for handling
-the tls-certificates interface.
+Example provider charm::
 
-## Getting Started
-
-From a charm directory, fetch the library using `charmcraft`:
-
-```shell
-charmcraft fetch-lib charms.tls_certificates_interface.v0.tls_certificates
-```
-
-You will also need to add the following library to the charm's `requirements.txt` file:
-- jsonschema
-
-### Provider charm
-Example:
-```python
-from charms.tls_certificates_interface.v0.tls_certificates import (
-    Cert,
-    TLSCertificatesProvides,
-)
-from ops.charm import CharmBase
+    from charmlibs.interfaces.tls_certificates.v0 import (
+        Cert,
+        TLSCertificatesProvides,
+    )
+    from ops.charm import CharmBase
 
 
-class ExampleProviderCharm(CharmBase):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.tls_certificates = TLSCertificatesProvides(self, "certificates")
-        self.framework.observe(
-            self.tls_certificates.on.certificates_request, self._on_certificate_request
-        )
+    class ExampleProviderCharm(CharmBase):
+        def __init__(self, *args):
+            super().__init__(*args)
+            self.tls_certificates = TLSCertificatesProvides(self, "certificates")
+            self.framework.observe(
+                self.tls_certificates.on.certificates_request, self._on_certificate_request
+            )
 
-    def _on_certificate_request(self, event):
-        common_name = event.common_name
-        sans = event.sans
-        cert_type = event.cert_type
-        certificate = self._generate_certificate(common_name, sans, cert_type)
+        def _on_certificate_request(self, event):
+            common_name = event.common_name
+            sans = event.sans
+            cert_type = event.cert_type
+            certificate = self._generate_certificate(common_name, sans, cert_type)
 
-        self.tls_certificates.set_relation_certificate(
-            certificate=certificate, relation_id=event.relation.id
-        )
+            self.tls_certificates.set_relation_certificate(
+                certificate=certificate, relation_id=event.relation.id
+            )
 
-    def _generate_certificate(self, common_name: str, sans: list, cert_type: str) -> Cert:
-        return Cert(
-            common_name=common_name, cert="whatever cert", key="whatever key", ca="whatever ca"
-        )
-```
+        def _generate_certificate(self, common_name: str, sans: list, cert_type: str) -> Cert:
+            return Cert(
+                common_name=common_name, cert="whatever cert", key="whatever key", ca="whatever ca"
+            )
 
-### Requirer charm
-Example:
+Example requirer charm::
 
-```python
-from charms.tls_certificates_interface.v0.tls_certificates import TLSCertificatesRequires
-from ops.charm import CharmBase
+    from charmlibs.interfaces.tls_certificates.v0 import TLSCertificatesRequires
+    from ops.charm import CharmBase
 
 
-class ExampleRequirerCharm(CharmBase):
-    def __init__(self, *args):
-        super().__init__(*args)
+    class ExampleRequirerCharm(CharmBase):
+        def __init__(self, *args):
+            super().__init__(*args)
 
-        self.tls_certificates = TLSCertificatesRequires(self, "certificates")
-        self.framework.observe(
-            self.tls_certificates.on.certificate_available, self._on_certificate_available
-        )
-        self.tls_certificates.request_certificate(
-            cert_type="client",
-            common_name="whatever common name",
-        )
+            self.tls_certificates = TLSCertificatesRequires(self, "certificates")
+            self.framework.observe(
+                self.tls_certificates.on.certificate_available, self._on_certificate_available
+            )
+            self.tls_certificates.request_certificate(
+                cert_type="client",
+                common_name="whatever common name",
+            )
 
-    def _on_certificate_available(self, event):
-        certificate_data = event.certificate_data
-        print(certificate_data["common_name"])
-        print(certificate_data["key"])
-        print(certificate_data["ca"])
-        print(certificate_data["cert"])
-```
-
+        def _on_certificate_available(self, event):
+            certificate_data = event.certificate_data
+            print(certificate_data["common_name"])
+            print(certificate_data["key"])
+            print(certificate_data["ca"])
+            print(certificate_data["cert"])
 """
 
 import json
