5a057d203fccdf3ee704b17c860930e01ef7c5af
James Garner <james.garner@canonical.com>
2025-09-09 18:13:54 +1200

    docs: avoid naming the testing framework that must not be named

--- .template/{{ cookiecutter.project_slug }}/tests/unit/test_charms.py
+++ .template/{{ cookiecutter.project_slug }}/tests/unit/test_charms.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""State-transition tests using Scenario (via ops.testing)."""
+"""Light weight state-transition tests of the library in a charming context."""
 
 import ops
 import ops.testing
