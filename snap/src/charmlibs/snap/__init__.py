b08972963a239062ffda9de816c804b4197691ee
James Garner <james.garner@canonical.com>
2025-09-16 17:37:10 +1200

    docs: fix package docstring formatting

--- snap/src/charmlibs/snap/__init__.py
+++ snap/src/charmlibs/snap/__init__.py
@@ -14,46 +14,42 @@
 
 """Representations of the system's Snaps, and abstractions around managing them.
 
-The `snap` package provides convenience methods for listing, installing, refreshing, and removing
+The ``snap`` package provides convenience methods for listing, installing, refreshing, and removing
 Snap packages, in addition to setting and getting configuration options for them.
 
-In the `snap` package, `SnapCache` creates a dict-like mapping of `Snap` objects at when
+In the ``snap`` package, ``SnapCache`` creates a ``dict``-like mapping of ``Snap`` objects when
 instantiated. Installed snaps are fully populated, and available snaps are lazily-loaded upon
-request. This module relies on an installed and running `snapd` daemon to perform operations over
-the `snapd` HTTP API.
+request. This module relies on an installed and running ``snapd`` daemon to perform operations over
+the ``snapd`` HTTP API.
 
-`SnapCache` objects can be used to install or modify Snap packages by name in a manner similar to
-using the `snap` command from the commandline.
+``SnapCache`` objects can be used to install or modify nnap packages by name in a manner similar to
+using the ``snap`` command from the commandline.
 
-An example of adding Juju to the system with `SnapCache` and setting a config value:
+An example of adding Juju to the system with ``SnapCache`` and setting a config value::
 
-```python
-try:
-    cache = snap.SnapCache()
-    juju = cache["juju"]
+    try:
+        cache = snap.SnapCache()
+        juju = cache["juju"]
 
-    if not juju.present:
-        juju.ensure(snap.SnapState.Latest, channel="beta")
-        juju.set({"some.key": "value", "some.key2": "value2"})
-except snap.SnapError as e:
-    logger.error("An exception occurred when installing charmcraft. Reason: %s", e.message)
-```
+        if not juju.present:
+            juju.ensure(snap.SnapState.Latest, channel="beta")
+            juju.set({"some.key": "value", "some.key2": "value2"})
+    except snap.SnapError as e:
+        logger.error("An exception occurred when installing charmcraft. Reason: %s", e.message)
 
-In addition, the `snap` module provides "bare" methods which can act on Snap packages as
+In addition, the ``snap`` module provides "bare" methods which can act on Snap packages as
 simple function calls. :meth:`add`, :meth:`remove`, and :meth:`ensure` are provided, as
-well as :meth:`add_local` for installing directly from a local `.snap` file. These return
-`Snap` objects.
+well as :meth:`add_local` for installing directly from a local ``.snap`` file. These return
+``Snap`` objects.
 
-As an example of installing several Snaps and checking details:
+As an example of installing several Snaps and checking details::
 
-```python
-try:
-    nextcloud, charmcraft = snap.add(["nextcloud", "charmcraft"])
-    if nextcloud.get("mode") != "production":
-        nextcloud.set({"mode": "production"})
-except snap.SnapError as e:
-    logger.error("An exception occurred when installing snaps. Reason: %s" % e.message)
-```
+    try:
+        nextcloud, charmcraft = snap.add(["nextcloud", "charmcraft"])
+        if nextcloud.get("mode") != "production":
+            nextcloud.set({"mode": "production"})
+    except snap.SnapError as e:
+        logger.error("An exception occurred when installing snaps. Reason: %s" % e.message)
 """
 
 from ._snap import (
