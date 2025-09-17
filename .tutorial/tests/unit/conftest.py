c0e4889bae169b3620d6d95de887a17613b60010
James Garner <james.garner@canonical.com>
2025-09-18 10:34:08 +1200

    docs: update tutorial docstring to match template

--- .tutorial/tests/unit/conftest.py
+++ .tutorial/tests/unit/conftest.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Fixtures for unit tests."""
+"""Fixtures for unit tests, typically mocking out parts of the external system."""
 
 import datetime
 
