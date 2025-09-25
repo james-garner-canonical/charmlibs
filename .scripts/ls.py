9b6d9ede2edb162e7f00dbba27c27db11dbb7341
James Garner <james.garner@canonical.com>
2025-09-25 21:19:04 +1200

    ci: remove duplicated _GLOBAL_FILES in ls.py

--- .scripts/ls.py
+++ .scripts/ls.py
@@ -33,7 +33,6 @@ import tarfile
 import tempfile
 from typing import Iterable, Iterator, Literal, Sequence
 
-_GLOBAL_FILES = ('.github', 'justfile', 'pyproject.toml')
 _REPO_ROOT = pathlib.Path(__file__).parent.parent
 _INTERFACES = _REPO_ROOT / 'interfaces'
 
