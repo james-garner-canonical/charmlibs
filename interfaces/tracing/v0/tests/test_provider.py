0ba98235434a605c1e07e1b6f2762ae77cfc38a7
James Garner <james.garner@canonical.com>
2025-09-30 14:47:33 +1300

    feat: add tracing interface spec to charmlibs monorepo (#185)
    This PR copies the [tracing interface definition from
    charm-relation-interfaces](https://github.com/canonical/charm-relation-interfaces/tree/main/interfaces/tracing)
    to this repo, under the `interfaces/tracing` directory. Some small
    automatic changes have been made, but have been kept to a minimum with
    the addition of a `ruff.toml` file to configure linting rules. One long
    comment line was manually broken.
    
    This PR will be merged without review so that we have a starting point
    of the interface definition as-is. Docs and tests can be updated as we
    add machinery to exercise them.

---
+++ interfaces/tracing/v0/tests/test_provider.py
@@ -0,0 +1,44 @@
+# Copyright 2023 Canonical
+# See LICENSE file for licensing details.
+
+from interface_tester.interface_test import Tester
+from scenario import Relation, State
+
+# No matter what's in the remote databags,
+# the tracing v1 provider will always populate with valid data.
+
+
+def test_data_on_created():
+    tester = Tester()
+    tester.run('foo-relation-created')
+    tester.assert_schema_valid()
+
+
+def test_data_on_joined():
+    tester = Tester()
+    tester.run('foo-relation-joined')
+    tester.assert_schema_valid()
+
+
+def test_data_on_changed():
+    tester = Tester()
+    tester.run('foo-relation-changed')
+    tester.assert_schema_valid()
+
+
+# also if the remote side sends rubbish
+def test_no_data_on_changed_bad_remote_data():
+    tester = Tester(
+        state_in=State(
+            relations=[
+                Relation(
+                    endpoint='foo',
+                    interface='tracing',
+                    remote_app_name='remote',
+                    remote_app_data={"bubble": "rubble"},
+                )
+            ]
+        )
+    )
+    tester.run('foo-relation-changed')
+    tester.assert_relation_data_empty()
---
+++ interfaces/tracing/v2/tests/test_provider.py
@@ -0,0 +1,63 @@
+# Copyright 2024 Canonical
+# See LICENSE file for licensing details.
+import json
+
+from interface_tester.interface_test import Tester
+from scenario import Relation, State
+
+_VALID_REQUIRER_APP_DATA = {
+    "receivers": json.dumps([
+        {"protocol": {"name": "otlp_grpc", "type": "grpc"}, "url": "http://192.0.2.0/24"}
+    ])
+}
+
+
+def test_data_on_created():
+    tester = Tester(
+        state_in=State(
+            relations=[
+                Relation(
+                    endpoint='tracing',
+                    interface='tracing',
+                    remote_app_name='remote',
+                    remote_app_data=_VALID_REQUIRER_APP_DATA,
+                )
+            ]
+        )
+    )
+    tester.run('tracing-relation-created')
+    tester.assert_schema_valid()
+
+
+def test_data_on_joined():
+    tester = Tester(
+        state_in=State(
+            relations=[
+                Relation(
+                    endpoint='tracing',
+                    interface='tracing',
+                    remote_app_name='remote',
+                    remote_app_data=_VALID_REQUIRER_APP_DATA,
+                )
+            ]
+        )
+    )
+    tester.run('tracing-relation-joined')
+    tester.assert_schema_valid()
+
+
+def test_data_on_changed():
+    tester = Tester(
+        state_in=State(
+            relations=[
+                Relation(
+                    endpoint='tracing',
+                    interface='tracing',
+                    remote_app_name='remote',
+                    remote_app_data=_VALID_REQUIRER_APP_DATA,
+                )
+            ]
+        )
+    )
+    tester.run('tracing-relation-changed')
+    tester.assert_schema_valid()
