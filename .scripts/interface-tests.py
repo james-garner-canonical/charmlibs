6c8bc46065d2c7b7886cfff60abca1d1086e243f
James Garner <james.garner@canonical.com>
2025-10-02 13:06:44 +1300

    wip: update interface-tests script to use forked interface_tester

--- .scripts/interface-tests.py
+++ .scripts/interface-tests.py
@@ -43,17 +43,58 @@ _INTERFACES = _REPO_ROOT / 'interfaces'
 _TEST_CONTENT = """
 # auto-generated test file
 
-from interface_tester import InterfaceTester
+# BEGIN: contents of {tests_path}
+
+{tests_content}
+
+# END: contents of {tests_path}
+
+import contextlib
+
+import interface_tester
+import pytest
 
 
-def test_{interface}_interface({fixture_id}: InterfaceTester):
+@pytest.fixture(scope="function", autouse=True)
+def test_{interface}_interface(
+    {fixture_id}: interface_tester.InterfaceTester,
+    request: pytest.FixtureRequest,
+):
     {fixture_id}.configure(
         interface_name="{interface}",
         interface_version={version},
         repo="{repo}",
         branch="{branch}",
+        interface_subdir="interface",
+        tests_dir="tests",
     )
-    {fixture_id}.run()
+    role = "{role}"
+    import schema
+    role_schema = getattr(schema, role.capitalize() + "Schema")
+    class Cleanup(Exception):
+        '''Exception to trigger context cleanup.'''
+    with contextlib.suppress(Cleanup):
+        with {fixture_id}.context(
+            test_fn=request.function,
+            role=role,
+            schema=role_schema,
+            endpoint="{endpoint}",
+        ):
+            yield
+            if getattr(request.node, 'report').failed:
+                raise Cleanup()
+""".strip()
+_PLUGIN_MODULE = 'report_plugin'
+_PLUGIN_CONTENT = """
+import pytest
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    print(type(outcome), outcome)
+    report = outcome.get_result()
+    if report.when == "call":
+        setattr(item, "report", report)
 """.strip()
 
 logging.basicConfig(level=logging.DEBUG)
@@ -65,7 +106,8 @@ def _main() -> None:
     parser.add_argument('name')
     parser.add_argument('version')
     parser.add_argument('role', choices=('provide', 'require'))
-    parser.add_argument('charm_name', nargs='?')
+    parser.add_argument('charm_name')
+    parser.add_argument('endpoint')
     parser.add_argument('--charm-repo')
     parser.add_argument('--charm-ref')
     parser.add_argument('--charmlibs-repo')
@@ -76,6 +118,7 @@ def _main() -> None:
         interface_version=args.version,
         role=args.role,
         charm_name=args.charm_name,
+        endpoint=args.endpoint,
         charm_repo=args.charm_repo,
         charm_ref=args.charm_ref,
         charmlibs_repo=args.charmlibs_repo,
@@ -89,6 +132,7 @@ def _interface_tests(
     interface_version: str,
     role: str,
     charm_name: str,
+    endpoint: str,
     charm_repo: str | None,
     charm_ref: str | None,
     charmlibs_repo: str | None,
@@ -99,6 +143,8 @@ def _interface_tests(
     charms = yaml.safe_load((interface_dir / 'interface.yaml').read_text())[f'{role}rs']
     [charm_config] = [c for c in charms if c['name'] == charm_name] or [{}]
     logger.info('Charm config: %s', charm_config)
+    if not charm_config:
+        assert charm_repo, f'--charm-repo is required as there is no config for {charm_name}.'
     test_config = charm_config.get('test_setup', {})
     # write and execute interface tester file in cloned charm repo
     with tempfile.TemporaryDirectory() as td:
@@ -111,18 +157,27 @@ def _interface_tests(
         logger.info(git_clone)
         subprocess.check_call(git_clone, cwd=td)
         # write interface test file
-        content = _TEST_CONTENT.format(
+        interface_tests_path = pathlib.Path(interface_dir, 'interface_tests',  f'test_{role}r.py')
+        charm_test_content = _TEST_CONTENT.format(
+            tests_path=interface_tests_path,
+            tests_content=interface_tests_path.read_text(),
             interface=interface_name,
             version=interface_version.removeprefix('v'),
             repo=charmlibs_repo or _REPO_ROOT,
             branch=charmlibs_ref or _current_branch(),
             fixture_id=test_config.get('identifier', 'interface_tester'),
+            role=f'{role}r',
+            endpoint=endpoint,
         )
         charm_root = repo_path / test_config.get('charm_root', '')
-        test_file = pathlib.Path(
-            test_config.get('location', 'tests/interface_tests/conftest.py')
-        ).parent / f'test_{role}s_{interface_name}_{interface_version}.py'
-        (charm_root / test_file).write_text(content)
+        charm_test_dir = (
+            charm_root
+            / test_config.get('location', 'tests/interface_tests/conftest.py')
+        ).parent
+        charm_test_file = charm_test_dir / f'test_{role}s_{interface_name}_{interface_version}.py'
+        charm_test_file.write_text(charm_test_content)
+        (charm_test_dir / 'schema.py').symlink_to(interface_dir / 'schema.py')
+        (charm_root / f'{_PLUGIN_MODULE}.py').write_text(_PLUGIN_CONTENT)
         # execute interface tests
         if pre_run := test_config.get('pre_run'):
             logger.info(pre_run)
@@ -132,13 +187,15 @@ def _interface_tests(
             'tool',
             'run',
             '--with=setuptools',
-            '--with=pytest-interface-tester',
+            '--with=git+https://github.com/james-garner-canonical/pytest-interface-tester@25-09+feat+location-customization-for-charmlibs',
             '--with-requirements=requirements.txt',
             'pytest',
-            test_file,
+            '-p',
+            _PLUGIN_MODULE,
+            charm_test_file,
         ]
         logger.info(pytest)
-        proc = subprocess.run(pytest, env={**os.environ, 'PYTHONPATH': 'src:lib'}, cwd=charm_root)
+        proc = subprocess.run(pytest, env={**os.environ, 'PYTHONPATH': '.:src:lib'}, cwd=charm_root)
         return proc.returncode
 
 
