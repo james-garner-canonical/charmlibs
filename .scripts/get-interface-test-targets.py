3103457e427ffb799b791924a822c09100a7d5a3
James Garner <james.garner@canonical.com>
2025-10-02 14:38:40 +1300

    ci: use subparsers for charm and interface sources

--- .scripts/get-interface-test-targets.py
+++ .scripts/get-interface-test-targets.py
@@ -40,21 +40,31 @@ logger = logging.getLogger(str(pathlib.Path(__file__).relative_to(_REPO_ROOT)))
 
 def _main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument('git_base_ref', nargs='?', default='')
-    parser.add_argument('--only-those-with-tests', action='store_true')
+    parser.add_argument('--all', action='store_true', help='Include combinations with no tests.')
+    subparsers = parser.add_subparsers(dest='source', required=True)
+    # from-interfaces
+    interfaces_parser = subparsers.add_parser('from-interfaces')
+    interfaces_parser.add_argument('git_base_ref', nargs='?', default='') 
+    # from-charm
+    charm_parser = subparsers.add_parser('from-charm')
+    charm_parser.add_argument('charm-repo')
+    charm_parser.add_argument('--charm-ref')
+    charm_parser.add_argument('--charm-name')
+    # run
     args = parser.parse_args()
-    targets = _get_interface_test_targets(
-        args.git_base_ref, has_tests_only=args.only_those_with_tests
-    )
+    if args.source == 'from-interfaces':
+        targets = _from_interfaces(changed_since=args.git_base_ref, has_tests_only=not args.all)
+    else:
+        raise NotImplementedError('WIP')
     output = json.dumps(targets)
     logger.info(output)
     print(output)
 
 
-def _get_interface_test_targets(git_base_ref: str, has_tests_only: bool) -> list[dict[str, str]]:
+def _from_interfaces(changed_since: str, has_tests_only: bool) -> list[dict[str, str]]:
     cmd = ['.scripts/ls.py', 'interfaces']
-    if git_base_ref:
-        cmd.append(git_base_ref)
+    if changed_since:
+        cmd.append(changed_since)
     interfaces = json.loads(subprocess.check_output(cmd, text=True).strip())
     targets: list[dict[str, str]] = []
     for i in interfaces:
