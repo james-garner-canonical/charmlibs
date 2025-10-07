40cad0fdcb3398e4f8d913008925cb36c44f02c9
James Garner <james.garner@canonical.com>
2025-10-07 13:20:58 +1300

    ci: run interface tests (#195)
    This PR adds the ability to run interface tests to our CI.
    
    ## Context
    
    The `charmlibs` monorepo is the new home for interface definitions and
    tests. We need to be able to execute existing interface tests in our CI.
    
    Interface test execution in `charm-relation-interfaces` is opaque and
    very abstract. I've attempted to improve this in a couple of PRs (see
    below), which have informed my approach here:
    - https://github.com/canonical/charm-relation-interfaces/pull/292)
    - https://github.com/canonical/charm-relation-interfaces/pull/297
    
    The approach is:
    1. Separate test setup, execution, and teardown from charm and test
    discovery.
    2. Run interface tests with `pytest` as directly as possible.
    3. Execute each `pytest` invocation in a separate job (one per
    `interface / version / role / charm / endpoint` combination).
    
    ## Overview
    
    This PR adds a new `interface-tests` workflow.
    
    - It runs interface tests for a single interface, totally compatible
    with existing interface tests and charm specific interface test
    configuration.
    - It's called from the `ci` workflow following the same approach as we
    use for the `tests` workflow.
    - It runs a separate matrix job for each `(version / role (provide or
    require) / charm / endpoint)` combination known for the interface.
    
    Two scripts have been added to support this workflow.
    - `.scripts/get-interface-test-targets.py` calculates the `(version /
    role / charm / endpoint)` targets for the matrix
    - `.scripts/run-interface-tests.py` runs the interface tests for a
    single target
    
    These scripts replace
    [run_matrix.py](https://github.com/canonical/charm-relation-interfaces/blob/main/run_matrix.py)
    from `charm-relation-interfaces` and the interface test and endpoint
    discovery and execution parts of
    [pytest-interface-tester](https://github.com/canonical/pytest-interface-tester/tree/main/interface_tester).
    
    ## Implementation details
    
    Interface tests are discovered in the local `charmlibs` repository,
    written to a cloned copy of the charm repo and then executed with
    `pytest`, using an `interface_tester` based fixture used to configure,
    setup, and teardown the interface tests (which are currently written
    using a pseudo-singleton
    [Tester](https://github.com/canonical/pytest-interface-tester/blob/main/interface_tester/interface_test.py#L175)
    class). In combination with execution in a separate Github job, this
    provides much better output in CI.
    
    Currently, the `interface_tester` library only provides the
    `InterfaceTester.run` entrypoint, which includes test discovery and
    execution. Instead of calling this, we write a fixture to the test file
    that calls the (new) `InterfaceTester.context` entrypoint, which handles
    configuration, setup and teardown of the `interface_tester.Tester`
    context. Currently, we install `pytest-interface-tester` from my fork,
    but when [this
    PR](https://github.com/canonical/pytest-interface-tester/pull/32) is
    merged, we can switch to `pytest-interface-tester~=3.4`.
    
    ## Test failures
    
    The tests for the `tracing` interface are currently failing. They're
    also [failing in
    charm-relation-interfaces](https://github.com/canonical/charm-relation-interfaces/actions/runs/18176547258/job/51743670143)
    with exactly the same (`scenario`) error -- so it's not related to this
    PR. This highlights the need to get interface tests running here
    alongside the interface libraries, and in charm repos.
    
    ## Future work
    
    A workflow to run interface tests from charm repos will be a separate
    PR. This should perhaps be in its own repository so that it can be
    versioned easily.

---
+++ .scripts/get-interface-test-targets.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env -S uv run --script --no-project
+
+# /// script
+# requires-python = ">=3.10"
+# dependencies = [
+#     "PyYAML",
+# ]
+# ///
+
+# Copyright 2025 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Collect the targets for running interface tests for the specified interface.
+
+Only outputs targets that there are test cases for by default.
+"""
+
+import argparse
+import json
+import logging
+import pathlib
+import re
+import subprocess
+import tempfile
+
+import yaml
+
+_REPO_ROOT = pathlib.Path(__file__).parent.parent
+
+logger = logging.getLogger(str(pathlib.Path(__file__).relative_to(_REPO_ROOT)))
+
+
+def _main() -> None:
+    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('interface', help='Path from repo root to specific interface directory.')
+    parser.add_argument('--all', action='store_true', help='Include combinations with no tests.')
+    args = parser.parse_args()
+    targets = _target_from_interface(args.interface, has_tests_only=not args.all)
+    output = json.dumps(targets)
+    logger.info(output)
+    print(output)
+
+
+def _target_from_interface(interface_str: str, has_tests_only: bool) -> list[dict[str, str]]:
+    """Return a list of interface test targets for this interface.
+
+    Iterates over the interface versions, collects all provider and requirer charms, and clones
+    the charms to discover their endpoints.
+
+    Args:
+        interface_str: The interface path relative to the repo root, e.g. 'interfaces/tracing'.
+        has_tests_only: If true, exclude any targets that don't actually have any tests defined.
+
+    Returns:
+        A list of interface test targets. Each target is a dictionary containing:
+            - interface (name)
+            - version (with v prefix)
+            - role (provide or require)
+            - charm (name)
+            - endpoint (name)
+    """
+    interface = _REPO_ROOT / interface_str
+    targets: list[dict[str, str]] = []
+    for v in sorted((interface / 'interface').glob('v[0-9]*')):
+        interface_yaml = yaml.safe_load((v / 'interface.yaml').read_text())
+        for role in 'provide', 'require':
+            charms = interface_yaml.get(f'{role}rs', [])
+            if not charms:
+                logger.debug('No charms for %s %s %s role.', interface_str, v.name, role)
+                continue
+            if has_tests_only and not _has_tests(v, f'{role}r'):
+                msg = 'Skipping these charms because there are no tests for %s %s %s: %s'
+                logger.warning(msg, interface_str, v.name, role, charms)
+                continue
+            for charm in charms:
+                endpoints = _get_endpoints(
+                    interface=interface.name,
+                    role_key=f'{role}s',
+                    charm_repo=charm['url'],
+                    charm_ref=charm.get('branch', 'main'),
+                    charm_root=charm.get('test_setup', {}).get('charm_root', ''),
+                )
+                targets.extend(
+                    {
+                        'interface': interface.name,
+                        'version': v.name,
+                        'role': role,
+                        'charm_name': charm['name'],
+                        'endpoint': endpoint,
+                    }
+                    for endpoint in endpoints
+                )
+    return targets
+
+
+def _has_tests(version_dir: pathlib.Path, role: str) -> bool:
+    """Return whether the test file exists and seems to contain at least one test.
+
+    We heuristically check for tests by looking for any line starting with 'def test',
+    ignoring leading whitespace.
+    """
+    test_file = version_dir / 'tests' / f'test_{role}.py'
+    if not test_file.exists():
+        return False
+    return bool(re.search(r'^\s*def test', test_file.read_text(), re.MULTILINE))
+
+
+def _get_endpoints(
+    interface: str, role_key: str, charm_repo: str, charm_ref: str | None, charm_root: str
+) -> list[str]:
+    """Clone the charm repo and return the endpoints for the interface and role."""
+    with tempfile.TemporaryDirectory() as td:
+        repo_path = pathlib.Path(td, 'charm-repo')
+        git_clone: list[str | pathlib.Path] = ['git', 'clone', '--depth', '1']
+        if charm_ref:
+            git_clone.extend(['--branch', charm_ref])
+        git_clone.extend([charm_repo, repo_path])
+        logger.info(git_clone)
+        subprocess.check_call(git_clone, cwd=td)
+        for meta in 'metadata.yaml', 'charmcraft.yaml':
+            if (path := repo_path / charm_root / meta).exists():
+                loaded = yaml.safe_load(path.read_bytes())
+                if role_key not in loaded:
+                    continue
+                endpoints = [e for e, d in loaded[role_key].items() if d['interface'] == interface]
+                if endpoints:
+                    return endpoints
+                raise ValueError(f'{interface} not found in {path}: {loaded[role_key]}')
+        msg = f'{role_key} {interface} not found in metadata for {charm_repo}@{charm_ref}/{charm_root}'  # noqa: E501
+        raise ValueError(msg)
+
+
+if __name__ == '__main__':
+    _main()
