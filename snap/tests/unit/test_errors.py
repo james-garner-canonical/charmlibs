6290f6db5450f1e400774e84e698a053e9eeff59
James Garner <james.garner@canonical.com>
2026-07-01 13:02:07 +1200

    docs: fix typo

--- snap/tests/unit/test_errors.py
+++ snap/tests/unit/test_errors.py
@@ -36,7 +36,7 @@ class TestErrorHierarchy:
 
     New error types will probably be specific snapd API errors, and should subclass APIError.
     This is what these tests expect by default. If a new error type is added elsewhere in the
-    error heirarchy, it should be added to _NON_API_ERROR_TYPES, and other tests added as needed.
+    error hierarchy, it should be added to _NON_API_ERROR_TYPES, and other tests added as needed.
     """
 
     def test_all_error_types_subclass_error(self, error_types: frozenset[type[BaseException]]):
